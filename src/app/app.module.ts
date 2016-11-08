40502f514a08a9c614359cb9b381fb0f5fcb6b47
Donato Laynes <donato.laynes@senial.net>
2016-11-07 21:52:01 -0500

    Diseño del home (static)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { QueHacerPage } from '../pages/que-hacer/que-hacer';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    QueHacerPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,10 +16,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    QueHacerPage
   ],
   providers: []
 })
--- src/pages/que-hacer/que-hacer.ts
+++ src/pages/que-hacer/que-hacer.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-quehacer',
+  templateUrl: 'que-hacer.html'
 })
-export class AboutPage {
+export class QueHacerPage {
 
   constructor(public navCtrl: NavController) {
 
