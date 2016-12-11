3d8554f49bab7a608ad4c91fa579b0c4bd69f01e
Donato Laynes <donato.laynes@senial.net>
2016-12-11 15:52:54 -0500

    Fixing config

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,6 +4,11 @@ export const ENV = {
   GOOGLE_API_KEY : ''
 };
 
+/* Este archivo ha sido removido del historial de git para cambios futuros,
+eso quiere decir, que la clave de google maps se encuentra protegida */
+//git update-index --assume-unchanged ./src/config/environment.ts
+//git update-index --no-assume-unchanged ./src/config/environment.ts
+
 /*
 export const ENV = {
   PRODUCTION : true,
